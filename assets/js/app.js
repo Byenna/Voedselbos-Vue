eb179f15231b8becc72c262f2c2194fbdbd5ab31
Juraia Pinas <byenna21@gmail.com>
2021-12-22 19:10:31 +0100

    added shopping-cart-items component

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,8 @@
 let app = new Vue({
     el: '#app',
     data: { 
-        fruitList: fruits,
-        cart: 0,
-        selectedVariants: [],
+        
+        
         shopCart: false,
         appName: 'Voedselbos',
         
@@ -14,26 +13,14 @@ let app = new Vue({
     },
 
     computed: {
-        name() {
-            return this.fruits[this.selectedVariants].name
-        },
-        price() {
-            return this.fruits[this.selectedVariants].price
-        }
+        
     },
 
     methods: {
         toggleCart() {
             this.shopCart = !this.shopCart
         },
-        updateCart(index) {
-            this.cart += 1
-            this.selectedVariants = index
-
-        },
-        removeItemFromCart(fruit) {
-            this.cart.splice(this.cart.indexOf(fruit))
-            }
+        
        
     }
 
