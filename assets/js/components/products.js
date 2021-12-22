ecbbf083bb5ab6312d6d0447a5d9833837ef5350
Juraia Pinas <byenna21@gmail.com>
2021-12-22 16:15:25 +0100

    added fruit data file and make product component

--- assets/js/components/products.js
+++ assets/js/components/products.js
@@ -1,100 +1,41 @@
-// Vue.component("products", {
-
-//     template: `
-//  <div id="allItems">
-//     <section id="form-container" class="itemsFirstRow">
-//       <div class="container">
-//         <div class="row">
-//           <div v-for="fruit in fruits" class="col-sm">
-//             <div class="card" style="width: 18rem;">
-//               <img :src="fruit.image" class="card-img-top" alt="kersen">
-//               <div class="card-body">
-//                 <h5 class="card-title">{{ fruit.name }}</h5>
-          
-//                 <p class="card-text">{{ fruit.description }}</p>
-//                 <h5 class="store-item-value">€ <strong id="store-item-price" class="font-weight-bold">4</strong>
-//                 </h5>
-          
-//                 <span class="buttonItem"><i  class="btn btn-primary">Bestel</i></span>
-          
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   </div>
-//     `,
-//     props: {
-//         products: {}
-//     },
-
-
-//     data() {
-//         return {
-//             fruits: [
-//                 {
-//                     id: 1,
-//                     name: "Zoete kersen",
-//                     description: "Kom zoveel kersen plukken als u wil voor een vaste prijs! De kersen zijn erg zoet dit jaar.",
-//                     image: "/assets/images/kersen.jpg",
-//                     price: 7,
-//                     quantity: 50,
-//                 },
-//                 {
-//                     id: 2,
-//                     name: "Sappige pruimen",
-//                     description: "Met deze pruimen maak je de lekkerste taart. De ladder staat al klaar. Waar wacht u nog op?",
-//                     image: "/assets/images/pruimen.jpg",
-//                     price: 4,
-//                     quantity: 20,
-//                 },
-//                 {
-//                     id: 3,
-//                     name: "Paprika",
-//                     description: "De paprika oogst is dit jaar wat magertjes. Graag niet meer dan 2 per klant.",
-//                     image: "/assets/images/paprika.jpg",
-//                     price: 2,
-//                     quantity: 10,
-//                 },
-//                 {
-//                     id: 4,
-//                     name: "Cherry tomaatjes",
-//                     description: "Deze cherrytomaatjes mogen niet ontbreken in uw salade. Zongerijpt en biologisch.",
-//                     image: "/assets/images/salade.jpg",
-//                     price: 5,
-//                     quantity: 70,
-//                 },
-//                 {
-//                     id: 5,
-//                     name: "Groentenpakket",
-//                     description: "Speciaal voor gasten die slecht ter been zijn maken wij ook een groentenpakket.",
-//                     image: "/assets/images/artisjok.jpg",
-//                     price: 8,
-//                     quantity: 50,
-//                 },
-//                 {
-//                     id: 6,
-//                     name: "Zure appels",
-//                     description: "Deze Granny's zijn lichtzuur van smaak en klaar om geplukt te worden.",
-//                     image: "/assets/images/appels.jpg",
-//                     price: 10,
-//                     quantity: 0,
-//                 }
-//             ],
-
-//         }
-//     },
-
-//     computed: {
-
-//     },
-
-//     methods: {
-
-//     },
+Vue.component("products", {
+    data: function () {
+        return {
+            fruitList: fruits
+        }
+    },
+    props: {
+        
+    },
 
 
 
-// })
-    
+    methods: {},
+
+    template: `
+<div  class="row">
+    <div  v-for="(fruit, index) in fruitList" class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img :src="fruit.image" class="card-img-top" alt="kersen">
+                <div class="card-body">
+                    <h5 class="card-title">{{ fruit.name }}</h5>
+
+                    <p class="card-text">{{ fruit.description }}</p>
+
+                    <h5 class="store-item-value">€ 
+                    <strong id="store-item-price" class="font-weight-bold">{{ fruit.price }}</strong>
+                    </h5>
+
+                    <span @click="updateCart(index)" class="buttonItem"><i
+                        class="btn btn-primary">Bestel</i>
+                    </span>
+                </div>
+        </div>
+    </div>
+</div>`,
+
+
+
+
+})
+
